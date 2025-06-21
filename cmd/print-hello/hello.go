ad2abc05f21cd9016130127a0eb35a64ca0f26da
Sun Ng <ng.winter2020@gmail.com>
2025-06-20 21:22:58 -0700

    Modified hello.go

--- cmd/print-hello/hello.go
+++ cmd/print-hello/hello.go
@@ -2,8 +2,14 @@ package mypackage
 
 import (
 	"fmt"
+
+	"github.com/agent3892/hello/mypackage"
 )
 
 func main() {
 	fmt.Println("Hello, Go!")
+
+	message := mypackage.Greet()
+	fmt.Println(message)
+
 }
